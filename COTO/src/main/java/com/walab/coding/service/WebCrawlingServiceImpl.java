4737130ed7f340c290b2ce77094f950e9237f242
YeeunJ <21800669@handong.edu>
2021-03-26 12:40:31 +0900

    [feat] webcrawling commit

--- COTO/src/main/java/com/walab/coding/service/WebCrawlingServiceImpl.java
+++ COTO/src/main/java/com/walab/coding/service/WebCrawlingServiceImpl.java
@@ -56,5 +56,39 @@ public class WebCrawlingServiceImpl implements WebCrawlingService {
 		
 		return problemInfo;
 	}
+	
+public List<ProblemDTO> crawlingLeetcodeByName(List<String> problem, int siteID) {
+		
+		CodingSiteDTO siteInfo = codingSitedao.readCodingSiteById(siteID);
+		List<ProblemDTO> problemInfo = new ArrayList<ProblemDTO>();
+		
+		for(String prob: problem) {
+			ProblemDTO pd = problemdao.readProblembyID(siteID, prob);
+			
+			if(pd == null) {
+				pd = new ProblemDTO();
+				String url = siteInfo.getMappingUrl().replace("${problem}", prob);
+				System.out.println(url);
+				System.out.println("----------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>===========");
+				try {
+					Document doc = Jsoup.connect(url).get();
+					System.out.println(prob+". "+doc.select("#problem_title").text());
+					pd.setName(prob+". "+doc.select("#problem_title").text());
+					pd.setLink(url);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("----------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>----------");
+				
+				pd.setSiteID(siteID);
+				pd.setSiteName(siteInfo.getSiteName());
+				pd.setSiteUrl(siteInfo.getSiteUrl());
+			}
+			problemInfo.add(pd);
+		}
+		
+		return problemInfo;
+	}
 }
 
